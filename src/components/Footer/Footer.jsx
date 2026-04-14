8c80af14aa9bf05a535e416c189c604261aa7846
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-14 07:55:38 +0600

    add responsivenes in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,13 +7,13 @@ import Image from 'next/image';
 
 const Footer = () => {
     return (
-        <div className='bg-[#244D3F] pt-10 '>
+        <div className='bg-[#244D3F] pt-15 '>
             <div className="max-w-11/12 mx-auto">
                 <Image src={logo} alt='logo footer' className='w-70 mx-auto' />
 
                 <p className='text-center text-white mt-3'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
-                <h3 className='text-center text-white mt-3 text-xl font-bold'>Social Links</h3>
-                <div className="flex justify-center items-center gap-3 mt-3">
+                <h3 className='text-center text-white mt-7 text-xl font-bold'>Social Links</h3>
+                <div className="flex justify-center items-center gap-3 mt-5">
                     <div className="bg-white w-12 h-12 rounded-full hover:scale-110 duration-700 cursor-pointer hover:-translate-y-1">
                         <Image src={instagram} alt='logo footer' className='' width={100} height={100} />
 
@@ -32,9 +32,9 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="border-t pt-8 pb-5 mt-8 border-[#ffffff30] md:flex justify-between items-center">
+                <div className="border-t pt-8 pb-5 mt-13 border-[#ffffff30] md:flex justify-between items-center">
                     <p className='text-white text-center md:text-start'>© 2026 KeenKeeper. All rights reserved.</p>
-                    <ul className='flex gap-3 justify-center md:justify-start text-white'>
+                    <ul className='flex gap-3 justify-center md:justify-start text-white mt-5 md:mt-0'>
                         <li>Privacy Policy</li>
                         <li>Terms of Service</li>
                         <li>Cookies</li>

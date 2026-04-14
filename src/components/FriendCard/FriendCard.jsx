66598e8ea3fc61db702189201ce32184ca2fc462
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-14 10:12:51 +0600

    friend card design finish

--- src/components/FriendCard/FriendCard.jsx
+++ src/components/FriendCard/FriendCard.jsx
@@ -1,11 +1,34 @@
+import Image from 'next/image';
 import React from 'react';
 
-const FriendCard = ({fri}) => {
+const FriendCard = ({ fri }) => {
+    const getDaysAgo = (date) => {
+        const today = new Date();
+        const lastDate = new Date(date);
+
+        const diffTime = today - lastDate; // milliseconds difference
+        const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+        return diffDays;
+    };
+
     console.log(fri)
     return (
-        <div className='card p-4 bg-white text-center'>
-            <h1 className='text-xl font-bold'>{fri.name }</h1>
-            <span className={` btn btn-sm capitalize rounded-full my-2  mx-auto text-white font-bold ${fri.status ==='overdue' && 'bg-[#EF4444]  '} ${fri.status ==='almost due' && 'bg-[#EFAD44]'} ${fri.status ==='on-track' && 'bg-[#244D3F]'}  `}>{fri.status}</span>
+        <div className='card p-4 bg-white text-center space-y-3'>
+
+            <Image src={fri.picture} alt='friend picture' width={100} height={100} className='w-25 mx-auto rounded-full '/>
+            <h1 className='text-xl font-bold'>{fri.name}</h1>
+
+            <p className='text-[#64748B] '>{getDaysAgo(fri.last_contact_date)}d ago</p>
+
+            <div className="flex justify-center gap-3 items-center">
+
+                {
+                    fri.tags.map((tag, i) => <button key={i} className='btn rounded-full btn-sm bg-[#CBFADB] w-25 text-[#244D3F] font-bold '>{tag}</button>)
+                }
+            </div>
+
+            <span className={` btn btn-sm capitalize rounded-full my-2  mx-auto text-white font-bold ${fri.status === 'overdue' && 'bg-[#EF4444]  '} ${fri.status === 'almost due' && 'bg-[#EFAD44]'} ${fri.status === 'on-track' && 'bg-[#244D3F]'}  `}>{fri.status}</span>
         </div>
     );
 };
